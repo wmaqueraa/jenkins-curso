7ebd88cda47303fbdee87cf0137efb1c043dc10d
Wilmer <wilmer.maquera@yahoo.com.pe>
2025-11-22 10:13:11 -0500

    Run test mejorado con Chatgpt

--- proyecto-pytest/run_test.sh
+++ proyecto-pytest/run_test.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+set -e 
 
 echo "activando el entorno virtual..."
 if [ ! -d "venv" ]; then
     python3 -m venv venv
 fi
-source venv/bin/activate
+#Activar entorno virtual (compatible con sh y bash)
+. venv/bin/activate
 
 echo "instalando dependencias....."
 pip install --upgrade pip
 pip install -r requirements.txt
 
+echo "Creando carpeta reports..."
+mkdir -p reports
+
 echo "ejecutando pruebas con Pytest......."
-pytest tests/ --junitxml=reports/test-result.xml --html=reports/test-results.html --self-contained-html
+venv/bin/pytest tests/ --junitxml=reports/test-result.xml --html=reports/test-results.html --self-contained-html
 
 echo "pruebas finalizadas resultados en reports" 
