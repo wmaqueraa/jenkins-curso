c735f8354f3659f0f57448f908a5737fa324b236
Wilmer <wilmer.maquera@yahoo.com.pe>
2025-11-22 09:21:38 -0500

    Correccion de Run_test

--- proyecto-pytest/run_test.sh
+++ proyecto-pytest/run_test.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 echo "activando el entorno virtual..."
+if[!"-d venv"]; then
+    python3 -m venv venv
+fi
 source venv/bin/activate
 
 echo "instalando dependencias....."
+pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "ejecutando pruebas con Pytest......."
